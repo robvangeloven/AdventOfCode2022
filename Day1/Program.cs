059a1cb6148c5e6032f61cb0245089ca223e26e0
Rob van Geloven <r4117314@dec-online.nl>
2022-12-01 11:34:25 +0100

    Add project files.

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+var elves = new List<Elf>();
+
+var currentElf = new Elf();
+
+foreach (var line in File.ReadLines("input.txt"))
+{
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        elves.Add(currentElf);
+        currentElf = new Elf();
+    }
+    else
+    {
+        currentElf.Calories.Add(int.Parse(line));
+    }
+}
+
+Console.WriteLine($"The {nameof(Elf)} with the most {nameof(Elf.Calories)} is carrying {elves.Max(elf => elf.Calories.Sum())} {nameof(Elf.Calories)}.");
+
+var topThreeElvesCalories = elves
+    .OrderByDescending(elf => elf.Calories.Sum())
+    .Take(3)
+    .Sum(elf => elf.Calories.Sum());
+
+
+Console.WriteLine($"The top three elves are carrying {topThreeElvesCalories} {nameof(Elf.Calories)}.");
+
+Console.ReadKey();
+
+internal record Elf
+{
+    public IList<int> Calories { get; } = new List<int>();
+}
