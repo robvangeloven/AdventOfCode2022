64ea88758ba964fd53a4bdee0101ed615a76c8ca
Rob van Geloven <r4117314@dec-online.nl>
2022-12-08 16:11:59 +0100

    Day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,32 @@
+﻿var part1 = File.ReadLines("input.txt")
+    .SelectMany(line =>
+    {
+        var compartmentSize = line.Length / 2;
+
+        var compartmentOne = line.Substring(0, compartmentSize);
+        var compartmentTwo = line.Substring(compartmentSize, compartmentSize);
+
+        var commonItems = compartmentOne
+        .Intersect(compartmentTwo)
+        .Select(commonItem => char.IsUpper(commonItem) ? commonItem - 'A' + 27 : commonItem - 'a' + 1);
+
+        return commonItems;
+    })
+    .Sum();
+
+var part2 = File.ReadLines("input.txt")
+    .Chunk(3)
+    .SelectMany(elfBags =>
+    {
+        var badgeValues = elfBags[0]
+        .Intersect(elfBags[1])
+        .Intersect(elfBags[2])
+        .Select(commonItem => char.IsUpper(commonItem) ? commonItem - 'A' + 27 : commonItem - 'a' + 1);
+
+        return badgeValues;
+    })
+    .Sum();
+
+Console.WriteLine($"Sum of priorities is: {part1}");
+Console.WriteLine($"Sum of badges is: {part2}");
+Console.ReadKey();
